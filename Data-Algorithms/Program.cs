7da3d04b4150347fe1971222e9f886d3750f2af3
sryalamanchi <sriniyalamanchi@live.com>
2021-02-19 20:42:38 -0500

    code.

--- Data-Algorithms/Program.cs
+++ Data-Algorithms/Program.cs
@@ -6,7 +6,21 @@ namespace Data_Algorithms
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Sorting Descending
+			int[] intArray = new int[] { 2, 3, 4, 5, 9 };
+			int temp = 0;
+			for (int i = 0; i <= intArray.Length - 1; i++)
+			{
+				for (int j = i + 1; j < intArray.Length; j++)
+				{
+					if (intArray[i] < intArray[j])
+					{
+						temp = intArray[i];
+						intArray[i] = intArray[j];
+						intArray[j] = temp;
+					}
+				}
+			}
 
 		}
 	}
